3a61ba85b8d3f8481435d5c27daa344cd11642f1
GHOST-Aram <owinoojala@gmail.com>
2024-07-04 16:03:31 +0300

    feat: Add page header

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,11 @@ function App() {
 
   return (
     <>
+    <header className="py-8 bg-orange-500 fixed top-0 left-0 right-0 z-10">
+	</header>
+    <main className="py-16">
       <ProductForm />
+    </main>
     </>
   )
 }
